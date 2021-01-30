518d500dd3098dc26159b7b86c35bc998d5247cb
Mirko <mirko.rinelli@gmail.com>
2021-01-30 10:46:02 +0100

    Snack 2

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
-<!-- PHP Snack 1:
-Creiamo un array 'matches' contenente altri array i
-quali rappresentano delle partite di basket di
-un’ipotetica tappa del calendario. Ogni array della
-partita avrà una squadra di casa e una squadra
-ospite, punti fatti dalla squadra di casa e punti fatti
-dalla squadra ospite.
-Stampiamo a schermo tutte le partite con questo
-schema:
-Olimpia Milano - Cantù | 55 - 60 -->
-
 <?php
+
+// PHP Snack 1:
+// Creiamo un array 'matches' contenente altri array i
+// quali rappresentano delle partite di basket di
+// un’ipotetica tappa del calendario. Ogni array della
+// partita avrà una squadra di casa e una squadra
+// ospite, punti fatti dalla squadra di casa e punti fatti
+// dalla squadra ospite.
+// Stampiamo a schermo tutte le partite con questo
+// schema:
+// Olimpia Milano - Cantù | 55 - 60
 
 $matches = [
     'firstMatch' => [
@@ -38,17 +38,32 @@ foreach ($matches as $key => $match) {
   $visitingScore = $match['visitingScore'];
   $hostScore = $match['hostScore'];
 
-  echo "$visitingTeam - $hostTeam | $visitingScore - $hostScore ";
+  echo "$visitingTeam - $hostTeam | $visitingScore - $hostScore" . "<br>";
 }
 
-?>
+echo "<br><hr><br>";
+// PHP Snack 2:
+// Passare come parametri GET name, mail e age e
+// verificare (cercando i metodi che non
+// conosciamo nella documentazione) che:
+// 1. name sia più lungo di 3 caratteri,
+// 2. mail contenga un punto e una chiocciola
+// 3. age sia un numero.
+// Se tutto è ok stampare “Accesso riuscito”, altrimenti
+// “Accesso negato”
 
-<!-- PHP Snack 2:
-Passare come parametri GET name, mail e age e
-verificare (cercando i metodi che non
-conosciamo nella documentazione) che:
-1. name sia più lungo di 3 caratteri,
-2. mail contenga un punto e una chiocciola
-3. age sia un numero.
-Se tutto è ok stampare “Accesso riuscito”, altrimenti
-“Accesso negato”  -->
+
+$name = $_GET['name'];
+$mail = $_GET['mail'];
+$age = $_GET['age'];
+
+//var_dump($name, $mail, $age);
+
+if (strlen($name) > 3 && strpos($mail, '@') && is_numeric($age)) {
+  echo "Accesso riuscito";
+} else {
+  echo "Accesso negato";
+}
+
+//?name=Mario&mail=mario.rossi@gmail.com&age=31 --> Paste into url
+?>
